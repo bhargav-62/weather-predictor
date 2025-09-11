83761de3f10951a657b170007a01e7b166153388
bhargav-62 <bhargav-62.bhargavvelagapudi9@gmail.com>
2025-09-11 21:48:45 +0530

    Use environment variable for API base URL

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = `${process.env.REACT_APP_API_URL}/api`;
+
 
 export const fetchWeather = async (location) => {
   const response = await axios.get(`${API_BASE}/weather`, { params: { location } });
